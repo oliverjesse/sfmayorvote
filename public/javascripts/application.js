9d5628ee42237a2b5dd50a527a4f41f261e45e18
Jesse Sanford <jesse@berkeley.edu>
2010-12-22 02:07:45 -0800

    working buttons; starting on sticky scroll

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+// When click a vote button
+// Append the current candidate to the val
+
+Array.prototype.last = function() {return this[this.length-1];}
+
+var	buttonHandler = function(e){
+  e.preventDefault();
+	// get this candidate's name
+	var candidate = $(e.target).attr("data-tag");
+	// get name array (from html? from server?)
+	var candidates = ["@DennisHerrera","@TomAmmiano","@BevanDufty","@MarkLeno","@ArtAgnos","@WillieBrown","MattGonzalez","EdHarrington","@DavidChiu","@RossMirkarimi","@MikeHennessey","@DavidCampos","@AaronPeskin"];
+
+	// get the tweet.
+	var tweet = $("#tbox iframe").contents().find("textarea").val();
+
+	// check for matches. if found, replace them with this name
+	var matched = 0;
+	_.each(candidates,function(c){
+		if (tweet.match(new RegExp(c))){
+			// alert(tweet);
+			$("#tbox iframe").contents().find("textarea").val(tweet.replace(new RegExp(c),candidate));			
+			matched = 1;
+		}
+	});	
+	if(matched == 0){
+		// append it to the textarea contents
+		$("#tbox iframe").contents().find("textarea").val(tweet + " " + candidate);			
+	}
+};
+
+$(document).ready(function(){
+	$(".vote-button").live("click", buttonHandler);
+	$(".small-vote-button").live("click", buttonHandler);
+	// $(".tbox_frame").stickyScroll({ container: $('#container') });
+});
---
+++ public/javascripts/jquery.stickyscroll.js
@@ -0,0 +1,89 @@
+/**
+ * StickyScroll
+ * written by Rick Harris - @iamrickharris
+ * 
+ * Requires jQuery 1.4+
+ * Disclaimer: Requires position: fixed support, so no IE6
+ * 
+ * Make elements stick to the top of your page as you scroll
+ *
+ * Usage: $(element).stickyScroll(options)
+ * 
+ * If the element is to be contained inside one of its parents, 
+ * (you want to keep it inside the main content area, and out of the header and footer)
+ * then provide a selector for that parent element
+ * Ex:
+ * $(element).stickyScroll({ container: '#my-wrapper' })
+ * 
+ * If you don't have a particular element you want the sticky element contained to,
+ * but still need control of its bottom boundary, use the manual mode
+ * Ex:
+ * $(element).stickyScroll({ mode: 'manual', 'bottomBoundary': 100 })
+ * and bottomBoundary is relative to the bottom of the document
+ * 
+*/
+
+(function($) {
+	$.fn.stickyScroll = function(options) {
+		
+		var settings = $.extend({
+				mode: 'auto', // 'auto' or 'manual', but anything other than auto will be treated as manual
+				container: $('body'),
+				bottomBoundary: null
+			}, options);
+			
+		// make sure user input is a jQuery object
+		settings.container = $(settings.container);
+		if(!settings.container.length) {
+			if(console) {
+				console.log('StickyScroll: the element ' + options.container + ' does not exist, we\'re throwing in the towel');
+			}
+			return;
+		}
+		
+		// calculate automatic bottomBoundary
+		if(settings.mode === 'auto') {
+			settings.bottomBoundary = $(document).height() - settings.container.offset().top - settings.container.attr('offsetHeight');
+		}
+		
+		return this.each(function() {
+			
+			var $this = $(this),
+				height = $this.attr('offsetHeight'),
+				topOffset = $this.offset().top,
+				leftOffset = $this.offset().left;
+			
+			function onScroll() {
+				var top = $(document).scrollTop(),
+					bottom = $(document).height() - top - height;
+					
+				if(bottom <= settings.bottomBoundary) {
+					$this.offset({ top: $(document).height() - settings.bottomBoundary - height, left: leftOffset });
+					settings.container.addClass('sticky-processed');
+				}
+				else if(top > topOffset) {
+					$this.css({
+						position: 'fixed',
+						left: leftOffset,
+						top: 0,
+						bottom: ''
+					});
+					settings.container.addClass('sticky-processed');
+				}
+				else if(top < topOffset) {
+					$this.css({
+						position: '',
+						left: '',
+						top: '',
+						bottom: ''
+					});
+					settings.container.removeClass('sticky-processed');
+				}
+			};
+			
+			onScroll();
+			$(window).scroll(onScroll);
+
+		});
+	};
+})(jQuery);
