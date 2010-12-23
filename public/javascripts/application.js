b2f4c87c3f57dcf1d33751b1c844c6c8728f0efd
Ben Woosley <ben.woosley@gmail.com>
2010-12-23 11:09:03 -0600

    Activate hovercards (and make js work with them)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,6 +9,9 @@ var buttonHandler = function(e){
   e.preventDefault();
   // get this candidate's name
   var candidate = $(e.target).data("tag");
+  if (candidate == undefined) {
+    candidate = $(e.target).parent().data('tag')
+  }
 
   var tweet_box = $("#tbox iframe").contents().find("textarea");
   // replace the contents with a vote
