ce910f3019e6c40ede133ad42f4db305a97ff34b
Ben Woosley <ben.woosley@gmail.com>
2010-12-23 10:59:44 -0600

    Fix the tweetbox text-replacement js to work (also much simpler)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,29 +10,11 @@ Array.prototype.last = function() {return this[this.length-1];}
 var	buttonHandler = function(e){
   e.preventDefault();
 	// get this candidate's name
-	var candidate = $(e.target).attr("data-tag");
-	// get name array (from html? from server?)
-	var candidates = ["@DennisHerrera","@TomAmmiano","@BevanDufty","@MarkLeno","@ArtAgnos","@WillieBrown","MattGonzalez","EdHarrington","@DavidChiu","@RossMirkarimi","@MikeHennessey","@DavidCampos","@AaronPeskin"];
+	var candidate = $(e.target).data("tag");
 
-	// get the tweet.
 	var tweet_box = $("#tbox iframe").contents().find("textarea");
-	var tweet = tweet_box.val();
-
-	// check for matches. if found, replace them with this name
-	var matched = false;
-	_.each(candidates,function(c){
-		if (tweet.match(new RegExp(c))){
-			// alert(tweet);
-			tweet_box.val(tweet.replace(new RegExp(c),candidate));
-			matched = true;
-		}
-	});
-	if (matched){
-		// append it to the textarea contents
-		// tweet_box.val(tweet + " " + candidate);
-		// replace the contents with a vote
-		tweet_box.val("@sfbos: I'd #vote " + candidate + " #sfmayor. How 'bout u? ow.ly/3tvum");
-	}
+	// replace the contents with a vote
+	tweet_box.val("@sfbos: I'd #vote " + candidate + " #sfmayor. How 'bout u? ow.ly/3tvum");
 };
 
 $(document).ready(function(){
