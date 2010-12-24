717d55c69e3b737060d951305563295a304058b4
Jesse Sanford <jesse@berkeley.edu>
2010-12-23 16:24:39 -0800

    Revert "Merge branch 'master' of github.com:jesseoliver/Markov into explosion"
    This reverts commit 2186b0f773cbdc2e389323af820066d9c52f5049, reversing
    changes made to 073f4ecd7af3cb7d1213e91a695a015ce885d0a3.

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -1,18 +1,23 @@
 module ChoicesHelper
-
+  
   def pretty_percent(c)
     number_to_percentage(c * 100, :precision => 0)
   end
-
+  
   def pretty_link(link)
     return "" unless link.present?
     link_to(" [more info] ",link)
   end
-
+  
   def rank(choice_counter)
     choice_counter + 1
   end
-
+  
+  def alpha(c)
+    return "alpha" if (c % 4 == 0)
+    ""
+  end
+  
   def choice_image_url(choice)
     "/images/politician-" + choice.name.split.last.downcase + ".jpg"
   end
