946ef616851f6faae38034559717803257b2ecd9
Jesse Sanford <jesse@berkeley.edu>
2010-12-23 16:13:23 -0800

    usable explosion display

--- public/javascripts/jquery-explosion.js
+++ public/javascripts/jquery-explosion.js
@@ -20,16 +20,18 @@ function explosion() {
    $('.inferno').scale(.5);
    $('.inferno').animate({scale: .8, opacity: 1}, 500, function() {
      $(this).animate({scale: .6, opacity: 0}, 4000);
-	 $(this).append('<div class="egg dust"></div>');
-	 $('.dust').css({height:200, width: 200, backgroundImage: 'url(/images/explosion_dust.png)', opacity:0});
+		 $('#logo').append('<div class="egg dust"></div>');
+		 $('.dust').css({height:200, width: 200, backgroundImage: 'url(/images/explosion_dust.png)', opacity:0});
      $('.dust').scale(1);
-     $('.dust').animate({scale: .5, opacity: 100}, 8000);
+     $('.dust').animate({scale: .5, opacity: 80}, 2000, function() {
+			 $('.dust').animate({scale: .5, opacity: 0}, 500);
+		 });
    });
    $('.burst').animate({opacity: 0});
    $('#logo').append('<div class="egg soil"></div>');
    $('.soil').css({height:200, width: 200, backgroundImage: 'url(/images/explosion_soil.png)'});
    $('.soil').scale(.5);
-   $('.soil').animate({scale: 1.7, opacity: 0}, 5000);
+   $('.soil').animate({scale: 1.7, opacity: 0}, 10000);
  });
 
 }
