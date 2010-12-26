b388b21092605cd2e06ed79e3172f6f017d4add9
Ben Woosley <ben.woosley@gmail.com>
2010-12-26 16:27:39 -0600

    Add some rescue clauses in the ezfilter script, with notification via hoptoad

--- script/ezfilter.rb
+++ script/ezfilter.rb
@@ -4,25 +4,33 @@ require 'twitter'
 USERNAME = "oliverjesse"  # Replace with your Twitter user
 PASSWORD = "FivnrQ12"  # and your Twitter password
 
-if args_start = ARGV.index('--')
-  USERNAME, PASSWORD = ARGV[args_start + 1].split(':')
-  tracks = (ARGV[args_start + 2 .. -1] + Chain.terms).flatten
-  Rails.logger.info "Starting a GrowlTweet to track: #{tracks.inspect}"
-end
+begin
+  if args_start = ARGV.index('--')
+    USERNAME, PASSWORD = ARGV[args_start + 1].split(':')
+    tracks = (ARGV[args_start + 2 .. -1] + Chain.terms).flatten
+    Rails.logger.info "Starting a GrowlTweet to track: #{tracks.inspect}"
+  end
 
-g = Growl.new 'localhost', 'growltweet', ['tweet'], ['tweet'], 'Owlgray'
+  g = Growl.new 'localhost', 'growltweet', ['tweet'], ['tweet'], 'Owlgray'
 
-Twitter.filter_stream(USERNAME, PASSWORD, { :keywords => tracks }) do |status|
-  Rails.logger.info "Waking up to smell the Tweet: #{status[:text]}\n"
-  voter = Voter.find_or_initialize_by_twitter_id(status[:user][:id])
-  voter.attributes = status[:user].slice(:name, :screen_name, :profile_image_url)
-  voter.save
-  tweet = voter.tweets.build(
-    'twitter_id' => status[:id],
-    'text' => status[:text]
-  )
-  tweet.save && (Rails.logger.info "Successfully saved Tweet for user #{status[:user][:screen_name]}\n")
+  Twitter.filter_stream(USERNAME, PASSWORD, { :keywords => tracks }) do |status|
+    begin
+      Rails.logger.info "Waking up to smell the Tweet: #{status[:text]}\n"
+      voter = Voter.find_or_initialize_by_twitter_id(status[:user][:id])
+      voter.attributes = status[:user].slice(:name, :screen_name, :profile_image_url)
+      voter.save
+      tweet = voter.tweets.build(
+        'twitter_id' => status[:id],
+        'text' => status[:text]
+      )
+      tweet.save && (Rails.logger.info "Successfully saved Tweet for user #{status[:user][:screen_name]}\n")
 
-  cached_index = Rails.root.join('public/index.html')
-  cached_index.unlink if cached_index.file?
+      cached_index = Rails.root.join('public/index.html')
+      cached_index.unlink if cached_index.file?
+    rescue Exception => e
+      HoptoadNotifier.notify(e)
+    end
+  end
+rescue Exception => e
+  HoptoadNotifier.notify(e)
 end
