8e6241dcc0fc7dd0fae952c294c42b1318d36ec1
Jesse Sanford <jesse@berkeley.edu>
2011-01-05 02:42:47 -0800

    add brock keeling

--- db/migrate/20110105100949_add_jan_fourth_writeins.rb
+++ db/migrate/20110105100949_add_jan_fourth_writeins.rb
@@ -2,6 +2,7 @@ class AddJanFourthWriteins < ActiveRecord::Migration
   def self.up
     Choice.create!(:chain_id => 1, :label => "Edwin Lee", :term => "EdLee @sfmayoredlee Lee", :link => "http://www.sfgate.com/cgi-bin/article.cgi?f=/c/a/2011/01/05/BAUB1H46CD.DTL&feed=rss.news")
     Choice.create!(:chain_id => 1, :label => "Frank Chu", :term => "FrankChu Frank Chu", :link => "http://en.wikipedia.org/wiki/Frank_Chu")
+    Choice.create!(:chain_id => 1, :label => "Brock Keeling", :term => "@brockder Brock Keeling BrockKeeling", :link => "http://www.sfweekly.com/bestof/2008/award/best-blogger-1033077/")
     
   end
 
